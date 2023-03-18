54ca7bf626a87df7e91015f3a54c6393b9026243
Stephanos Kuma <stephanos@kuma.ai>
2023-03-18 22:12:38 +0000

    Add the base command
    All commands should subclass this class.

---
+++ src/phosphorus/commands/base.py
@@ -0,0 +1,21 @@
+from __future__ import annotations
+
+from argparse import Namespace
+from typing import cast
+
+from phosphorus._seven import toml_parser
+from phosphorus.lib.metadata import Metadata
+
+
+class BaseCommand:
+    __slots__ = ["meta"]
+
+    def __init__(self, _args: Namespace, /):
+        self.meta = Metadata.from_path()
+
+    def __call__(self) -> None:
+        raise NotImplementedError(f"{self.__class__.__qualname__} must be callable.")
+
+    def get_current_hash(self) -> str:
+        with self.meta.lockfile.open("rb") as file:
+            return cast(str, toml_parser(file)["phosphorus"]["meta"]["hash"])

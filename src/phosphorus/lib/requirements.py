604994f72a7fc5d4a47e23026abfd0e3be912705
Stephanos Kuma <stephanos@kuma.ai>
2023-03-27 23:24:11 +0100

    Add a class to hold requirement info
    It also gets all the release numbers

---
+++ src/phosphorus/lib/requirements.py
@@ -0,0 +1,79 @@
+from __future__ import annotations
+
+import re
+from dataclasses import dataclass
+
+from phosphorus.lib.markers import Marker
+from phosphorus.lib.packages import Package
+from phosphorus.lib.regex import requirement_pattern
+from phosphorus.lib.versions import Version, VersionClause
+
+
+@dataclass(frozen=True, order=True)
+class Requirement:
+    package: Package
+    clauses: tuple[VersionClause, ...]
+    marker: Marker
+
+    @classmethod
+    def from_string(cls, requirement: str) -> Requirement:
+        clause, *markers = requirement.split(";", maxsplit=1)
+        marker = (
+            Marker.from_string(markers[0].strip())
+            if markers
+            else Marker(boolean=None, markers=())
+        )
+        full_match = re.fullmatch(requirement_pattern, clause.strip())
+        if not full_match:
+            msg = "Could not parse the requirement"
+            raise RuntimeError(msg)
+        name = full_match["name"].strip()
+        if full_match["clauses"]:
+            clauses = tuple(
+                VersionClause.from_string(matched_clause)
+                for matched_clause in full_match["clauses"].strip(" ()").split(",")
+            )
+        else:
+            clauses = ()
+        return cls(package=Package(name=name), clauses=clauses, marker=marker)
+
+    def __str__(self) -> str:
+        name = self.package.name
+        clauses = ",".join(str(clause) for clause in self.clauses)
+        marker = str(self.marker)
+        if clauses and marker:
+            return f"{name} ({clauses}) ; {marker}"
+        if clauses:
+            return f"{name} ({clauses})"
+        if marker:
+            return f"{name} ; {marker}"
+        return name
+
+    def get_versions(
+        self,
+        *,
+        enforce_pep440: bool,
+        allow_pre_releases: bool,
+        allow_dev_releases: bool,
+    ) -> list[Version]:
+        dependencies = self.package.get_dependency_info()
+        versions = []
+        for release in dependencies["releases"]:
+            version = Version.from_string(release)
+            if version.is_pre_release and not allow_pre_releases:
+                continue
+            if version.is_dev_release and not allow_dev_releases:
+                continue
+            if not version.pep_440_compliant and enforce_pep440:
+                continue
+            for clause in self.clauses:
+                if not clause.match(version):
+                    continue
+            versions.append(version)
+        return sorted(versions, reverse=True)
+
+
+@dataclass(frozen=True, order=True)
+class RequirementGroup:
+    group: str
+    requirements: tuple[Requirement, ...]

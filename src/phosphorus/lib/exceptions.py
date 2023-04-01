05b379bd9e61df3918960621232c2fe7a923eb07
Stephanos Kuma <stephanos@kuma.ai>
2023-04-01 10:05:46 +0100

    Add the install command

--- src/phosphorus/lib/exceptions.py
+++ src/phosphorus/lib/exceptions.py
@@ -4,3 +4,7 @@ class UnreachableCodeError(AssertionError):
 
 class MissingProjectRootError(RuntimeError):
     """The project root could not be found"""
+
+
+class PipError(RuntimeError):
+    """Pip exited unsuccessfully"""

defbf0ee48c4ccae3d7a67cdf348b2b5e597ff21
Stephanos Kuma <stephanos@kuma.ai>
2023-04-03 13:59:22 +0100

    Add a class to perform matching against a version

--- src/phosphorus/lib/constants.py
+++ src/phosphorus/lib/constants.py
@@ -1,4 +1,36 @@
+from __future__ import annotations
+
+from enum import Enum, unique
 from pathlib import Path
 
 package_cache = Path.home().joinpath(".cache/phosphorus/packages/")
 pypi_cache = package_cache.joinpath("PyPI")
+
+
+@unique
+class ComparisonOperator(Enum):
+    EXACT_MATCH = "==="
+    EQUAL_TO = "=="
+    NOT_EQUAL = "!="
+    COMPATIBLE_WITH = "~="
+    LESS_OR_EQUAL = "<="
+    GREATER_OR_EQUAL = ">="
+    LESS_THAN = "<"
+    GREATER_THAN = ">"
+
+    def __str__(self) -> str:
+        return self.value
+
+    @classmethod
+    def prefix_match_operator(cls) -> set[ComparisonOperator]:
+        return {cls.EQUAL_TO, cls.NOT_EQUAL}
+
+    @classmethod
+    def non_local_operator(cls) -> set[ComparisonOperator]:
+        return {
+            cls.LESS_THAN,
+            cls.LESS_OR_EQUAL,
+            cls.GREATER_OR_EQUAL,
+            cls.GREATER_THAN,
+            cls.COMPATIBLE_WITH,
+        }

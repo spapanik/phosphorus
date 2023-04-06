fb6528c00cb4eaf76a9c7ae6d48a3e2e9edb713a
Stephanos Kuma <stephanos@kuma.ai>
2023-04-06 10:51:24 +0100

    Add a class to hold contributor data

---
+++ src/phosphorus/lib/contributors.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from collections.abc import Iterable
+from dataclasses import dataclass
+from typing import Self
+
+
+@dataclass(frozen=True, slots=True, order=True)
+class Contributor:
+    name: str
+    email: str
+
+    @classmethod
+    def from_data(cls, data: dict[str, str]) -> Self:
+        return cls(name=data.get("name", ""), email=data.get("email", ""))
+
+    def formatted_email(self) -> str:
+        if not self.email:
+            return ""
+
+        if not self.name:
+            return self.email
+
+        return f'"{self.name}" <{self.email}>'
+
+    @staticmethod
+    def stringify_names(contributors: Iterable[Contributor]) -> str:
+        return ",".join(
+            contributor.name for contributor in contributors if contributor.name
+        )
+
+    @staticmethod
+    def stringify_emails(contributors: Iterable[Contributor]) -> str:
+        return ",".join(
+            contributor.formatted_email()
+            for contributor in contributors
+            if contributor.email
+        )

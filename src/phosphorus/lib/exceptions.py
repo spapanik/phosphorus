0a88183aeccaf5a6ff39bf069f8844174d881cef
Stephanos Kuma <stephanos@kuma.ai>
2024-11-11 12:18:21 +0000

    ✨ Add a python runner that runs in activated python

--- src/phosphorus/lib/exceptions.py
+++ src/phosphorus/lib/exceptions.py
@@ -36,3 +36,11 @@ class UVError(RuntimeError):
         command_string = shlex.join(command_parts)
         msg = f"uv command `{command_string}` exited with a non-zero status code"
         super().__init__(msg)
+
+
+class PythonSubprocessError(RuntimeError):
+    """python as a subprocess exited unsuccessfully"""
+
+    def __init__(self, python_binary: str, command: str) -> None:
+        msg = f"python command `{command}` exited with a non-zero status code when run with `{python_binary}`"
+        super().__init__(msg)
--- src/phosphorus/lib/subprocess.py
+++ src/phosphorus/lib/subprocess.py
@@ -5,10 +5,20 @@ from typing import TYPE_CHECKING
 
 from uv import find_uv_bin
 
-from phosphorus.lib.exceptions import UVError
+from phosphorus.lib.exceptions import PythonSubprocessError, UVError
 
 if TYPE_CHECKING:
     from subprocess import CompletedProcess
+
+
+def find_python_bin() -> str:
+    return (
+        run(  # noqa: PLW1510, S603
+            ["which", "python"], capture_output=True  # noqa: S607
+        )
+        .stdout.decode()
+        .strip()
+    )
 
 
 def uv_run(args: list[str], *, verbose: bool) -> CompletedProcess[bytes]:
@@ -20,4 +30,16 @@ def uv_run(args: list[str], *, verbose: bool) -> CompletedProcess[bytes]:
         if verbose:
             print(output.stderr.decode())
         raise UVError(*command)
+    return output
+
+
+def python_run(command_string: str, *, verbose: bool) -> CompletedProcess[bytes]:
+    python_bin = find_python_bin()
+    command = [python_bin, "-c", command_string]
+    output = run(command, capture_output=True)  # noqa: PLW1510, S603
+    if output.returncode:
+        print()
+        if verbose:
+            print(output.stderr.decode())
+        raise PythonSubprocessError(python_bin, command_string)
     return output
